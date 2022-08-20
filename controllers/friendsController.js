7a58f74a873b8bbd9df2c692718bc4757405db2e
Mika.dev <charmikadev@gmail.com>
2022-08-20 16:26:30 +0100

    fixed send_request method, implemented accept_request_post method using express-validator

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const async = require('async');
 
 exports.send_request_post = [
-    body('request_id', 'ID Must not be empty').trim().isLength({min: 1}).escape(),
+    body('requested_id', 'ID Must not be empty').trim().isLength({min: 1}).escape(),
     (req, res, next) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -14,8 +14,7 @@ exports.send_request_post = [
 
         jwt.verify(req.token, process.env.JWT_KEY, (err, authData) => {
             if (err) return res.json({error: err, message: "JWT Auth Error"});
-            const requested_id = req.body.request_id;
-            console.log(`req id 2: ${requested_id}`);
+            const requested_id = req.body.requested_id;
             async.parallel({
                 updated_requested(callback) {
                     User.findByIdAndUpdate(requested_id,
@@ -35,22 +34,45 @@ exports.send_request_post = [
     }
 ]
 
-exports.accept_request_post = (req, res, next) => {
-    async.parallel({
-        updated_requested(callback) {
-            User.findByIdAndUpdate(requested_id,
-                {$pull: {friends_requested: sender_id}},
-                {$push: {friends: sender_id}},
-                {}).exec(callback);
-        },
-        updated_sender(callback) {
-            User.findByIdAndUpdate(sender_id,
-                {$pull: {friends_requested: requested_id}},
-                {$push: {friends: sender_id}},
-                {}).exec(callback);
+
+exports.accept_request_post = [
+    body('sender_id', 'ID Must not be empty').trim().isLength({min: 1}).escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            res.json({error: errors, message: "Form validation error"});
+            return;
         }
-    }, (err, results) => {
-        if (err) return res.json({error: err, message: "Error updating users"});
-        res.json({message: "Users updated"})
-    })
-}
+
+        jwt.verify(req.token, process.env.JWT_KEY, (err, authData) => {
+            if (err) return res.json({error: err, message: "JWT Auth Error"});
+            const sender_id = req.body.requested_id;
+            async.parallel({
+                updated_requested(callback) {
+                    User.findByIdAndUpdate(authData._id,
+                        {$pull: {friends_requested: sender_id}},
+                        {}).exec(callback);
+                },
+                updated_requested_friends(callback) {
+                    User.findByIdAndUpdate(authData._id,
+                        {$push: {friends: sender_id}},
+                        {}).exec(callback);
+                },
+                updated_sender(callback) {
+                    User.findByIdAndUpdate(sender_id,
+                        {$pull: {friends_requested: authData._id}},
+                        {}).exec(callback);
+                },
+                updated_sender_friends(callback) {
+                    User.findByIdAndUpdate(sender_id,
+                        {$push: {friends: authData._id}},
+                        {}).exec(callback);
+                }
+            }, (err, results) => {
+                if (err) return res.json({error: err, message: "Error updating users"});
+                res.json({message: "Users updated"})
+            })
+        })
+    }
+]
